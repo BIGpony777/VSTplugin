6236919e83aa0b337e80e5d8c7afabcccc11e789
Ryan Bradley <90108067+BIGpony777@users.noreply.github.com>
2023-01-08 01:09:48 -0500

    Create params.h

---
+++ params.h
@@ -0,0 +1,19 @@
+#pragma once
+
+enum {
+	kOsc1 = 100,
+	kOsc2,
+	kOsc3,
+	kSlide1,
+	kPit1,
+	kPit2,
+	kPit3
+};
+
+#define default_Osc1 0.8
+#define default_Osc2 0.8
+#define default_Osc3 0.8
+#define default_Slide1 0.1
+#define default_Pit1 0.1
+#define default_Pit2 0.1
+#define default_Pit3 0.1
